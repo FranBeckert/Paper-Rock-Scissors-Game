29e8559d92a65f59114883f639284097e98dcfa0
bouacha mouhamed neddjmeddine <141820410+Bouacha23@users.noreply.github.com>
2025-01-28 19:48:59 +0200

    " create playRound  function "

--- external.js
+++ external.js
@@ -1,7 +1,31 @@
 // Function to generate a random option for the computer
+const options = ["rock", "paper", "scissors"];
 const computerPlay = () => {
-  const options = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * options.length);
   console.log(options[randomIndex]);
   return options[randomIndex];
 };
+
+
+function playRound(playerSelection, computerSelection) {
+
+  playerSelection = playerSelection.toLowerCase();
+  if(!options.includes(playerSelection)) {
+    return "error"
+  }
+  if (playerSelection == computerSelection) {
+      return "It's a tie!";
+  } else if (
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")||
+    (playerSelection == "rock" && computerSelection == "scissors") 
+  ) {
+      return "You Win!";
+  } else {
+      return "You Lose!";
+  }
+}
+
+const playerSelection = 'rock' ; 
+const computerSelection = computerPlay() ; 
+console.log(playRound(playerSelection ,computerSelection)) ; 
